a935846efd28e545b94b34693aa4b32930633420
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-11 13:51:37 +0300

    Incapsulation style demo.

--- src/main/java/demo/Logger.java
+++ src/main/java/demo/Logger.java
@@ -4,18 +4,31 @@ package demo;
  * Created by eugene on 11/01/2017.
  */
 public class Logger {
+    private FileSaver saver = new FileSaver("tttt");
+    private LevelFilter filter = new LevelFilter();
+
     public void log(String message, int level) {
-        if (filter(level)) {
-            save(message);
+        if (filter.filter(level)) {
+            saver.save(message);
         }
     }
+}
 
-    private void save(String message) {
+class FileSaver {
+    private final String pathToFile;
 
+
+    public FileSaver(String pathToFile) {
+        this.pathToFile = pathToFile;
     }
 
-    private boolean filter(int level) {
+    public void save(String message) {
+        //.....
+    }
+}
+
+class LevelFilter {
+    public boolean filter(int level) {
         return false;
     }
-
 }
